b779765301f2cd82f55f82911b050d1c28824f3d
sbhbenjamin <sbhbenjamin@gmail.com>
2022-06-30 14:20:16 +0800

    add theme text

--- components/about/Theme.js
+++ components/about/Theme.js
@@ -8,11 +8,29 @@ const Theme = () => {
         <h2>Theme</h2>
         <div className={styles.themeText}>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
-            facilis pariatur ut quidem ad libero ex impedit tempora consequuntur
-            quibusdam, corrupti nesciunt maxime eveniet suscipit quis eligendi
-            voluptas veniam! Esse. Corrupti nesciunt maxime eveniet suscipit
-            quis eligendi voluptas veniam! Esse.
+            To our Industry Partners, Staff, Alumni, and Students: For the first
+            time since the pandemic began, the NUS M.Arch grad show is back
+            physically! Our theme this year, &ldquo;Architecture
+            Etcetera&rdquo;, focuses on celebrating the designer behind the work
+            just as much as the work itself! Glimpses of life and interests
+            outside and amongst the field of architecture will be interspersed
+            throughout the show.
+          </p>
+          <p>
+            This year’s works are grouped into five clusters: (i) Atmosphere +
+            Agency (ii) Conservation + Heritage (iii) Performative Design (iv)
+            Speculative Environments (v) Urban Commons + Wellness
+          </p>
+          <p>
+            These clusters represent the main research branches of this
+            year&apos;s cohort. The show recognises that there is no one
+            methodology that can best articulate what architecture truly is;
+            these trajectories offer a mere glimpse into the futures crafted by
+            the graduating cohort. Culminating our thesis semester, the cohort
+            of 2022 would like to invite all to join us in the celebration of
+            our thesis works through our physical exhibition, publications, as
+            well as website. Stay tuned for more details as we approach event
+            day itself!
           </p>
         </div>
       </div>
