a35710e8f66ba74e8bb3f0ceeaa664e622513f5f
sbhbenjamin <sbhbenjamin@gmail.com>
2022-07-02 18:07:47 +0800

    add active state to menu tab

--- components/MenuTab.js
+++ components/MenuTab.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/MenuTab.module.css';
+import { useRouter } from 'next/router';
 
 const MenuTab = ({ closeTab }) => {
+  const router = useRouter();
   return (
     <>
       <span onClick={closeTab} className={styles.overlayTab} />
@@ -10,17 +12,23 @@ const MenuTab = ({ closeTab }) => {
         <div className={`${styles.content}`}>
           <Link href="/index" passHref>
             <a>
-              <h3>Works</h3>
+              <h3 className={router.pathname === '/index' && styles.active}>
+                Works
+              </h3>
             </a>
           </Link>
           <Link href="/about" passHref>
             <a>
-              <h3>About</h3>
+              <h3 className={router.pathname === '/about' && styles.active}>
+                About
+              </h3>
             </a>
           </Link>
           <Link href="/book" passHref>
             <a>
-              <h3>Book</h3>
+              <h3 className={router.pathname === '/book' && styles.active}>
+                Book
+              </h3>
             </a>
           </Link>
         </div>
