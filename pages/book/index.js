deff08dc13e4fe856629556c6ffceb6afca5137a
sbhbenjamin <sbhbenjamin@gmail.com>
2022-07-03 00:24:54 +0800

    fix broken static assets and update book description

--- pages/book/index.js
+++ pages/book/index.js
@@ -13,7 +13,7 @@ const index = () => {
           <div className={styles.bookContent}>
             <div className={styles.bookImage}>
               <Image
-                loader={() => '/placeholder.png'}
+                loader={() => '/2022/placeholder.png'}
                 src="/placeholder.png"
                 alt="NUS M.Arch II Book"
                 height={390}
@@ -27,12 +27,42 @@ const index = () => {
                   <em>A compendium of works for the batch of 2022</em>
                 </p>
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Aspernatur facilis pariatur ut quidem ad libero ex impedit
-                  tempora consequuntur quibusdam, corrupti nesciunt maxime
-                  eveniet suscipit quis eligendi voluptas veniam! Esse. Corrupti
-                  nesciunt maxime eveniet suscipit quis eligendi voluptas
-                  veniam! Esse.
+                  The NUS M.Arch Grad Show Book 2022 celebrates the achievements
+                  of our students&apos; year-long design leadership in their
+                  thesis work, and it is a compendium of radical and thoughtful
+                  ideas for the architecture discipline as well as the larger
+                  society. As part of our larger NUS M.Arch Show 2022, the
+                  cohort&apos;s work are loosely located into five threads, each
+                  addressing a relevant discourse in today&apos;s context. These
+                  five threads offer a glimpse of the diverse yet collective
+                  voice of the thesis projects, our students, and our
+                  architecture education in the Department of Architecture, NUS.
+                </p>
+                <p>
+                  Hard copies of the book are now available, going at the order
+                  price of SGD65. It is a hard cover, 565 pages compendium of
+                  thesis descriptions and visuals, with accompanying artefacts
+                  such as interviews and reflections on research and living
+                  through the pandemic. Please note that the book is free for
+                  AY21/22 M.Arch II students/M.Arch teaching faculty as well as
+                  industry members who have RSVP-ed for the opening night of
+                  Gradshow 2022 (one copy per company).
+                </p>
+                <p>
+                  Unfortunately, the book will only be available for local
+                  (Singapore) purchase at this point of time. Please fill in the
+                  form below if you wish to order, and make the payment through
+                  PayLah!/PayNow to Jeremy &lt;+65 9183 9791&gt;. We will only
+                  confirm the order once we have received your payment. Once the
+                  books are ready for collection, we will use your contact
+                  details to notify you of the collection time, date and
+                  location.
+                </p>
+                <p>
+                  <strong>
+                    As there are limited copies available, this order form will
+                    close by 24th July 2022 or if orders have been met.
+                  </strong>
                 </p>
               </div>
 
